cd418601a8e7b857d8bd5cc0bd1bed8b780d1c08
John Foster <john@varyn.com>
2020-08-09 14:14:00 -0700

    Update test bootstrap for local testing

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,4 +3,6 @@
 define('TEST_ROOT', __DIR__);
 define('TEST_RESOURCES_ROOT', __DIR__ . '/resources');
 
+require_once __DIR__ . '/../Michelf/Markdown.inc.php';
+require_once __DIR__ . '/../Michelf/MarkdownExtra.inc.php';
 require_once __DIR__ . '/helpers/MarkdownTestHelper.php';
